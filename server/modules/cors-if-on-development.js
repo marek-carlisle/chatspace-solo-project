8d7cac733f611e1bcb5af9ee87732a7d46d6892f
Luke Schlangen <lukeschlangen@gmail.com>
2018-01-10 17:21:24 -0600

    add warning for missing environment variable in dev

--- server/modules/cors-if-on-development.js
+++ server/modules/cors-if-on-development.js
@@ -17,8 +17,23 @@ const corsOptions = {
 };
 
 // middleware function that allows CORS so localhost:3000 can make requests while in development
-const runCors = cors(corsOptions);
+const runCors = function() {
+  // Warning if user is using CORS, but doesn't have REACT_APP_API_BASE_URL
+  if(!process.env.REACT_APP_API_BASE_URL) {
+    console.log(`
+    ----------------------------
+  
+    *** WARNING ***
+    To run in develop mode, you need REACT_APP_API_BASE_URL
+    You don't have REACT_APP_API_BASE_URL
+    Please follow the README and add a .env file  
+    
+    ----------------------------`);
+  }
 
-const middleware = isDevelopmentEnvironment ? runCors : doNothing;
+  return cors(corsOptions);
+};
+
+const middleware = isDevelopmentEnvironment ? runCors() : doNothing;
 
 module.exports = middleware;
