be70553f4f12b76629ddbe289d1f56fda3403998
Luke Schlangen <lukeschlangen@gmail.com>
2018-04-27 11:13:22 -0500

    remove unused warning

--- server/constants/warnings.js
+++ server/constants/warnings.js
@@ -15,20 +15,9 @@ add or change your SERVER_SESSION_SECRET environment variable!
 
 ----------------------------`;
 
-const noBaseApiUrl = `
-----------------------------
-
-*** WARNING ***
-To run in develop mode, you need REACT_APP_API_BASE_URL
-You don't have REACT_APP_API_BASE_URL
-Please follow the README and add a .env file  
-
-----------------------------`;
-
 const exampleBadSecret = 'superDuperSecret';
 
 module.exports = {
   badSecret,
-  noBaseApiUrl,
   exampleBadSecret,
 };
