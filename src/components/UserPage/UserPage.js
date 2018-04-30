6b9582539e88a7f8a18a1521efa08b0cbfd1085d
Dane Smith <doctorhowser@gmail.com>
2018-04-30 08:47:42 -0500

    move files into component folders;

---
+++ src/components/UserPage/UserPage.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Nav from '../../components/Nav/Nav';
+
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
+import { LOGIN_ACTIONS } from '../../redux/actions/loginActions';
+
+
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+class UserPage extends Component {
+  componentDidMount() {
+    this.props.dispatch({
+      type: USER_ACTIONS.FETCH_USER
+    });
+  }
+
+  componentDidUpdate() {
+    if (!this.props.user.isLoading && this.props.user.userName === null) {
+      this.props.history.push('home');
+    }
+  }
+
+  logout = () => {
+    this.props.dispatch({
+      type: LOGIN_ACTIONS.LOGOUT
+    });
+    // this.props.history.push('home');
+  }
+
+  render() {
+    let content = null;
+
+    if (this.props.user.userName) {
+      content = (
+        <div>
+          <h1
+            id="welcome"
+          >
+            Welcome, { this.props.user.userName }!
+          </h1>
+          <button
+            onClick={this.logout}
+          >
+            Log Out
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <Nav />
+        { content }
+      </div>
+    );
+  }
+}
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(UserPage);
+
