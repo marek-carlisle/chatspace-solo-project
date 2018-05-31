6a40de7580ece02bae78be31db8c00ef8c2f0076
Kris Szafranski <kris@thoughtshelter.com>
2018-05-31 16:46:01 -0500

    removed password from select statement

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -8,7 +8,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  pool.query('SELECT * FROM person WHERE id = $1', [id]).then((result) => {
+  pool.query('SELECT id, username FROM person WHERE id = $1', [id]).then((result) => {
     // Handle Errors
     const user = result && result.rows && result.rows[0];
 
