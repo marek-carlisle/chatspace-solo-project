597540c423a17a4ec0d8e4eb3ce871bf3c0f7645
Dane Smith <doctorhowser@gmail.com>
2018-09-20 15:49:17 -0500

    add more comments to redux files

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -6,7 +6,7 @@ import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
-
+// Instead of taking everything from state, we just want the user info.
 const mapStateToProps = state => ({
   user: state.user,
 });
