3392e7a7a7a4be04cfd98dd1dfe0850db65c556a
Luke Schlangen <lukeschlangen@gmail.com>
2018-10-22 16:48:24 -0500

    remove unused logout method

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 
-class UserPage extends Component {
-  logout = () => {
-    this.props.dispatch({ type: 'LOGOUT' });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 id="welcome">
-          Welcome, { this.props.user.username }!
-        </h1>
-        <p>Your ID is: {this.props.user.id}</p>
-        <LogOutButton className="log-in" />
-      </div>
-    );
-  }
-}
+// this could also be written with destructuring parameters as:
+// const UserPage = ({ user }) => (
+// and then instead of `props.user.username` you could use `user.username`
+const UserPage = (props) => (
+  <div>
+    <h1 id="welcome">
+      Welcome, { props.user.username }!
+    </h1>
+    <p>Your ID is: {props.user.id}</p>
+    <LogOutButton className="log-in" />
+  </div>
+);
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
@@ -29,4 +24,3 @@ const mapStateToProps = state => ({
 
 // this allows us to use <App /> in index.js
 export default connect(mapStateToProps)(UserPage);
-
