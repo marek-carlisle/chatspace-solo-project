f017b4456d8af478eded709a62af17eb34f4d73b
Dane Smith <doctorhowser@gmail.com>
2018-12-17 09:35:02 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,3 +3,9 @@ CREATE TABLE "person" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "pet" (
+    "id" SERIAL PRIMARY KEY,
+    "firstname" VARCHAR (80) UNIQUE NOT NULL,
+    "person_id" INT REFERENCES "person"
+);
