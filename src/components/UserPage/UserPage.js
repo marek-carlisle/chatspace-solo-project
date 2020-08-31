a3254bed7d8185207083d3dc1960ebd565b8d74a
Myron <dubtwee@gmail.com>
2020-08-31 09:44:36 -0500

    correcting props for the user page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -4,18 +4,17 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class UserPage extends Component {
-
   // this component doesn't do much to start, just renders some user info to the DOM
-  render(props) {
+  render() {
     return (
       <div>
-        <h1 id="welcome">Welcome, {props.store.user.username}!</h1>
-        <p>Your ID is: {props.store.user.id}</p>
+        <h1 id="welcome">Welcome, {this.props.store.user.username}!</h1>
+        <p>Your ID is: {this.props.store.user.id}</p>
         <LogOutButton className="log-in" />
       </div>
-    )
+    );
   }
-};
+}
 
 // this allows us to use <App /> in index.js
 export default connect(mapStoreToProps)(UserPage);
