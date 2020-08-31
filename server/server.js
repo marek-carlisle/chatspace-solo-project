25cbe681184f8a94cc5f9ab409078a0d52933144
Myron <dubtwee@gmail.com>
2020-08-31 15:07:14 -0500

    adding shelf router to the server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 require('dotenv').config();
 
@@ -10,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const shelfRouter = require('./routes/shelf.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/shelf', shelfRouter);
 
 // Serve static files
 app.use(express.static('build'));
