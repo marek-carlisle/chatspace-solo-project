ededb3b9710fba916a9c817603cfd0febe21b662
Myron <dubtwee@gmail.com>
2020-08-31 15:52:14 -0500

    adjusting session secrect to use a static secret

--- server/modules/session-middleware.js
+++ server/modules/session-middleware.js
@@ -14,14 +14,17 @@ const warnings = require('../constants/warnings');
 */
 
 const serverSessionSecret = () => {
-  if (!process.env.SERVER_SESSION_SECRET ||
-      process.env.SERVER_SESSION_SECRET.length < 8 ||
-      process.env.SERVER_SESSION_SECRET === warnings.exampleBadSecret) {
+  if (
+    !process.env.SERVER_SESSION_SECRET ||
+    process.env.SERVER_SESSION_SECRET.length < 8 ||
+    process.env.SERVER_SESSION_SECRET === warnings.exampleBadSecret
+  ) {
     // Warning if user doesn't have a good secret
     console.log(warnings.badSecret);
   }
 
-  return process.env.SERVER_SESSION_SECRET;
+  // return process.env.SERVER_SESSION_SECRET;
+  return '902384uroiwefaj9oq238u9or3ijwefl8u49rw8uefijljwuei';
 };
 
 module.exports = cookieSession({
@@ -30,5 +33,5 @@ module.exports = cookieSession({
   resave: 'false',
   saveUninitialized: false,
   maxAge: 60 * 60 * 1000, // Set to 1 hour - 60 min/hour * 60 s/min * 1000 ms/s
-  secure: false
+  secure: false,
 });
