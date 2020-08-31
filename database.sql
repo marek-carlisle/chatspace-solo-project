1c94c00c38634f6008268f2678ed69f7c9b5a0cf
Myron <dubtwee@gmail.com>
2020-08-31 15:08:17 -0500

    adding SQL queries for the shelf assignment

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-
 -- USER is a reserved keyword with Postgres
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
@@ -7,4 +6,11 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "item" (
+    "id" SERIAL PRIMARY KEY,
+    "description" VARCHAR (80) NOT NULL,
+    "image_url" VARCHAR (2083),
+    "user_id" INT REFERENCES "user"
 );
