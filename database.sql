f07178020165f1a4d76a4a6ac27e002bcd69d468
Sevisis Maverick <Mavericksevisis@gmail.com>
2020-10-27 14:34:15 -0500

    server routers complete

--- database.sql
+++ database.sql
@@ -3,8 +3,84 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- CREATE TABLES!!!
+
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "first_name"  VARCHAR (40) NOT NULL,
+  "middle_name" VARCHAR (40), -- Client NEEDS to send an EMPTY STRING if user DOES NOT input this OPTIONAL input
+  "last_name" VARCHAR (40) NOT NULL,
+  "date_of_birth" DATE, --  "date_of_birth" ?
+  "date_of_register" TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- "date_of_register" ?
+  "last_logged_in" TIMESTAMP,
+  "username" VARCHAR (40) UNIQUE NOT NULL,
+  "password" VARCHAR (80) NOT NULL,
+  "moderator" BOOLEAN DEFAULT FALSE,
+  "admin" BOOLEAN DEFAULT FALSE
 );
+
+CREATE TABLE "channels" ( -- Every channel will load a set of messages with the respective "channel_id"
+"id" SERIAL PRIMARY KEY, 
+"channel_name" VARCHAR (40) NOT NULL
+);
+
+CREATE TABLE "messages" (
+"id" SERIAL PRIMARY KEY,
+"user_id" INT REFERENCES "users", -- "user_id" will come from whichever user "id" sent it
+"channel_id" INT REFERENCES "channels", -- "channel_id" will come from whichever channel "id" it was sent from
+"message_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- NEED A TIMESTAMP
+"message" VARCHAR (480) NOT NULL -- The message itself that every user will submit into a channel
+);
+
+CREATE TABLE "image_list" (
+  "id" SERIAL PRIMARY KEY,
+  "image_title"  VARCHAR (400),
+  "image_description" VARCHAR (400),
+  "image_url" VARCHAR (400)
+  );
+
+
+-- DROP TABLES!!!
+
+DROP TABLE "users";
+
+DROP TABLE "channels";
+
+DROP TABLE "messages";
+
+-- INSERTS!!!
+
+INSERT INTO "users" ("first_name", "middle_name", "last_name", "date_of_birth", "username", "password", "moderator", "admin")
+VALUES 
+('Marek', 'Mason', 'Carlisle', '1997-08-18', 'Sevisis Maverick', 'ThatYukiCat2019', TRUE, TRUE),
+('Undefined', NULL, 'Undefined', NULL, 'Savis Undefined', 'RulerofEverything1', TRUE, FALSE),
+('Vex', NULL, 'Auric', NULL, 'Vex of The Void', 'DarknessIsMyDestiny', FALSE, FALSE);
+
+SELECT * FROM "users";
+
+INSERT INTO "channels" ("channel_name")
+VALUES
+('main_chat'),
+('media_chat');
+
+SELECT * FROM "channels";
+
+INSERT INTO "messages" ("user_id", "channel_id", "message")
+VALUES
+('1', '1', 'Hello world!'),
+('2', '1', 'Hello universe.'),
+('1', '2', 'Well send media in here!'), -- I have that quotation problem again lmao, Blaine help!
+('2', '2', 'Sure whatever, have some cat pics.'),
+('2', '2', '*Savis proceeds to send cat pics.*');
+
+SELECT * FROM "messages";
+
+INSERT INTO "image_list" ("image_title", "image_description", "image_url")
+VALUES
+('Calico', 'Almost all calico cats are female', 'https://bit.ly/35AyyAP'),
+('Tabby', 'My first cat was an orange tabby named Maverick', 'https://bit.ly/37CQtte');
+
+SELECT * FROM "image_list";
+
+
+-- JOIN THE TABLES!!!
---
+++ server/routes/chat.router.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+// GET messages route
+router.get('/messages', (req, res) => {
+  console.log('Retrieving stuff from prime_app database');
+  let queryText = (`SELECT * FROM "messages";`)
+  pool.query(queryText).then(result => {
+    res.send(result.rows);
+  })
+    .catch(error => {
+      console.log('Error obtaining messages', error);
+      res.sendStatus(418);
+    });
+});
+
+// POST messages route
+router.post('/postmessage', rejectUnauthenticated, (req, res) => {
+  console.log('Adding message into prime_app database', req.user, req.body);
+  let queryText = `INSERT INTO "messages" ("user_id", "channel_id", "message")
+  VALUES ($1, $2, $3);`;
+  pool.query(queryText, [req.user.id, req.body.channel, req.body.message])
+    .then(result => {
+      res.sendStatus(200)
+    })
+    .catch(err => {
+      console.log('Error posting message', err);
+      res.sendStatus(418);
+    });
+});
+
+// DELETE messages route
+router.delete('/deletemessage/:id', rejectUnauthenticated, (req, res) => {
+  console.log(req.user, req.params);
+  console.log(`Deleting message with ID ${req.params.id} by user ID ${req.user.id} from prime_app database`);
+  let queryText = `DELETE FROM "messages" WHERE "id" = $2 AND "user_id" = $1`;
+  pool.query(queryText, [req.user.id, req.params.id])
+    .then(result => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      console.log('Error deleting message', err);
+      res.sendStatus(418);
+    });
+});
+
+//PUT messages route
+router.put('/editmessage/:id', rejectUnauthenticated, (req, res) => {
+  console.log(`Editing message with ID ${req.params.id} by user ID ${req.user.id} from prime_app database`);
+  let queryText = `UPDATE "messages" SET "message"=$3 WHERE "id"=$2 AND "user_id" = $1`;
+  pool.query(queryText, [req.user.id, req.params.id, req.body.message])
+    .then(result => {
+      res.sendStatus(204);
+    })
+    .catch(err => {
+      console.log('Error editing message', err);
+      res.sendStatus(418);
+    });
+});
+
+
+
+module.exports = router;
